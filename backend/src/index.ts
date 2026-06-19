2afa971525c29c35abea7379eba159cb4d54f9e3
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2026-06-19 16:53:04 -0300

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,10 @@ import sendRouter from "./routes/Send.routes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*'
+}));
+
 app.use(express.json());
 
 app.use(
